bb10880885ec05264b8bf141af2eceebfbc28f67
rafimd422 <rafimf2222@gmail.com>
2023-11-26 21:37:09 +0600

    created a post request on our social login button

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,4 +1,5 @@
 import { AuthContext } from "@/Provider/AuthProvider";
+import useAxiosPublic from "@/hooks/useAxiosPublic";
 import { useRouter } from "next/router";
 import { useContext } from "react";
 import GoogleButton from "react-google-button";
@@ -7,21 +8,40 @@ import swal from "sweetalert";
 export default function SocialLogin() {
   const { googleSignIn } = useContext(AuthContext);
   const router = useRouter();
+  const axiosPublic = useAxiosPublic()
+
+
   const handleGoogleLogin = () => {
     googleSignIn()
-      .then(() => {
-        if (router.pathname !== "/auth/signin") {
-          router.push(router.pathname);
-        } else {
-          router.push("/");
-        }
-        swal("Logged In!", "Successfully!", "success");
-
+      .then(result => {
+        console.log(result.user);
+  
+        const userData = {
+          email: result.user?.email,
+          image: result.user?.photoURL,
+          name: result.user?.displayName
+        };
+  
+        axiosPublic.post('/user', userData)
+          .then(res => {
+            console.log(res.data);
+            if (router.pathname !== "/auth/signup") {
+              router.push(router.pathname);
+            } else {
+              router.push("/");
+            }
+            swal("Logged In!", "Successfully!", "success");
+          })
+          .catch(error => {
+            swal("Error", error.message, "error");
+          });
       })
       .catch(error => {
         swal("Error", error.message, "error");
       });
   };
+  
+  
 
   return (
     <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
